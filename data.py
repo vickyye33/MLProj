bb2c3f25038f6ac341a96d8349ae4ad6fecf3da3
vickyye33 <49155808+vickyye33@users.noreply.github.com>
2025-04-13 17:51:17 +0800

    机器学习-海浪预测
    批量读取fub文件及数据;提取netcdf文件中的指定经纬度的时序数据

---
+++ data.py
@@ -0,0 +1,24 @@
+
+#from xml.dom.minidom import parse
+import xml.etree.ElementTree as ET
+import csv
+#from lxml import etree
+
+# 定义解析器
+#parser = etree.XMLParser(encoding="utf-8")
+
+# 解析 XML 文件
+#tree = etree.parse("2023/01/202301010000MF01001.dat.xml", parser=parser)
+
+# 查看解析结果
+#print(etree.tostring(tree, encoding='utf-8').decode('utf-8'))
+
+# 使用 XPath 提取信息
+#locs = tree.xpath('2023/01/202301010000MF01001.dat.xml')
+#print(locs)
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    
