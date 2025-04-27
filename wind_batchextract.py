3428693ec92ecd08f4c28bf0756b2aad107601b0
evaseemefly-t5820 <evaseemefly@126.com>
2025-04-27 22:34:56 +0800

    title:25-04-27 wind_batchextract.py 完成了 1:批量读取nc文件并提取指定经纬度的72小时数据并拼接成 dataframe -> def batch_readncfiles_byyears; 2:将上面step1处理的按月保存的72小时浮标站位的时序数据合并为一整年的数据

--- wind_batchextract.py
+++ wind_batchextract.py
@@ -1,4 +1,5 @@
 import csv
+from typing import Optional
 
 import arrow
 import numpy as np
@@ -103,6 +104,76 @@ def batch_readncfiles(read_path: str, lat: float, lng: float, month: int = 1):
     return df
 
 
+def batch_readncfiles_byyears(read_path: str, lat: float, lng: float, year: int = 2024, month: Optional[int] = None):
+    """
+        根据指定路径遍历该路径下的所有文件，并读取每个文件中的[0,23]h的时序数据(根据经纬度)
+    :param read_path: 读取nc的根目录
+    :param lat:
+    :param lng:
+    :return:
+    """
+
+    df_nc: xr.Dataset = None
+    nc_path = pathlib.Path(read_path)
+    df_dict: dict = {}
+    out_put_path: str = r'G:\05DATA\01TRAINING_DATA\WIND'
+    for file in nc_path.iterdir():
+        # for file in 'GRAPES_2024010100_240h_UV.nc','GRAPES_2024010112_240h_UV.nc':
+        # 修改为按月切分
+        if file.is_file():
+            try:
+                # 获取当前文件的 datetime 字符串
+                dt_str: str = file.name.split('_')[1]
+                dt_arrow = arrow.get(dt_str, 'YYYYMMDDHH')
+                temp_year = dt_arrow.year
+                temp_month = dt_arrow.month
+                if year == temp_year:
+                    if month is not None:
+                        if temp_month == month:
+                            # step1: 拼接成文件全路径
+                            # file_full_path = nc_path / file
+                            file_full_path_str: str = str(file)
+                            # step2: 使用 xarray.open_dataset 打开 netcdf文件
+                            temp_df: xr.Dataset = xr.open_dataset(file_full_path_str)
+                            # 注意: 打开的 Dataset 有三个维度,目前只需要按照经纬度提取几个位置的24h内的时序数据
+                            if temp_df is not None:
+                                """
+                                    Coordinates:
+                                    * latitude             (latitude) float64 -89.94 -89.81 -89.69 ... 89.81 89.94
+                                    * longitude            (longitude) float64 0.0 0.125 0.25 ... 359.8 359.9
+                                    * time                 (time) datetime64[ns] 2024-01-01 ... 2024-01-11
+                                    Data variables:
+                                        UGRD_10maboveground  (time, latitude, longitude) float32 ...
+                                        VGRD_10maboveground  (time, latitude, longitude) float32 ...
+                                """
+                                # 从该文件中提取指定经纬度的时序数据
+                                filter_ds = temp_df.sel(latitude=lat, longitude=lng, method='nearest')
+                                # 分别取出 u 与 v 分量
+                                u_vals = filter_ds['UGRD_10maboveground'].values[:25]
+                                v_vals = filter_ds['VGRD_10maboveground'].values[:25]
+                                dt_vals = filter_ds['time'].values
+                                dt64_forecast_start = dt_vals[0]
+                                dt_forecast_start: datetime = pd.to_datetime(dt64_forecast_start)
+                                dt_forecast_start_str: str = dt_forecast_start.strftime('%Y%m%d%H%M%S')
+                                temp_u_column_name: str = f'{dt_forecast_start_str}_u'
+                                temp_v_column_name: str = f'{dt_forecast_start_str}_v'
+                                df_dict[temp_u_column_name] = u_vals
+                                df_dict[temp_v_column_name] = v_vals
+                                print(f"读取{file_full_path_str}成功")
+                            else:
+                                df_nc = temp_df
+            except Exception as e:
+                print(e.args)
+    # 将最终的 dict -> pd.DataFrame
+    df = pd.DataFrame(df_dict)
+    print('生成最终DataFrame ing ')
+    out_put_filename: str = f'out_put_MF01001_{str(year)}_{str(month)}.csv'
+    save_path: str = str(pathlib.Path(out_put_path) / out_put_filename)
+    df.to_csv(save_path)
+    print(f'存储路径:{save_path}')
+    # return df
+
+
 def simulate_forcast_data():
     """
         测试生成三个浮标数据
@@ -116,7 +187,7 @@ def simulate_forcast_data():
     pass
 
 
-def batch_get_realdata(file_full_path: str, split_hours=72, issue_hours_steps: int = 12):
+def batch_get_realdata(file_full_path: str, split_hours=72, issue_hours_steps: int = 12) -> pd.DataFrame:
     """
         TODO:[-] 25-04-23 生成实况训练数据集
         从指定文件批量获取时间数据并以dataframe的形式返回
@@ -131,6 +202,7 @@ def batch_get_realdata(file_full_path: str, split_hours=72, issue_hours_steps: i
                         YYYYMMDDHHmm
     """
     list_series = []
+    merge_dict = {}
     if pathlib.Path(file_full_path).exists():
         # ds: xr.Dataset = xr.open_dataset(file_full_path)
         df: pd.DataFrame = pd.read_csv(file_full_path)
@@ -158,7 +230,18 @@ def batch_get_realdata(file_full_path: str, split_hours=72, issue_hours_steps: i
             temp_index: int = df_reindexed.index.get_loc(temp_time)
             val_series = df_reindexed[temp_index:temp_index + split_hours]
             list_series.append(val_series)
-    return list_series
+        # TODO:[-] 25-04-24 此处做重新修改，拼接成一个dataframe
+
+        for temp_time in issue_dt_series:
+            dt_str: str = temp_time.strftime('%Y%m%d%H%M%S')
+            temp_index: int = df_reindexed.index.get_loc(temp_time)
+            val_series = df_reindexed[temp_index:temp_index + split_hours]
+            # 此处改为只取 'WS' 列
+            # TODO:[-] 25-04-24 住一次此处需要将每一个 series的index索引重置为 [0,71]
+            merge_dict[dt_str] = val_series['WS'].reset_index(drop=True)
+            # list_series.append(val_series)
+    df = pd.DataFrame.from_dict(merge_dict)
+    return df
 
 
 def traning():
@@ -209,15 +292,52 @@ def traning():
     print(f"Predicted YBG values: {ybg_prediction}")
 
 
+def merge_dataframe(read_path: str) -> Optional[pd.DataFrame]:
+    """
+        读取指定路径，遍历该路径下的所有文件，将读取的dataframe横向拼接
+    :param read_path:
+    :return:
+    """
+    first_df: Optional[pd.DataFrame] = None
+    if pathlib.Path(read_path).exists():
+        for file in pathlib.Path(read_path).iterdir():
+            temp_df: pd.DataFrame = pd.read_csv(str(file))
+            if first_df is None:
+                first_df = temp_df
+            else:
+                first_df = pd.concat([first_df, temp_df], axis=1)
+    return first_df
+
+
 def main():
     read_path: str = r'Z:\WIND\GRAPES\2024'
     out_put_path: str = r'./data'
     out_put_file_path: str = str(pathlib.Path(out_put_path) / 'GRAPES_2024_24')
     lat: float = 39.5003
     lng: float = 120.59533
+    """
+        TODO:[*] 25-04-24 ERROR:
+            KeyError: [<function _open_scipy_netcdf at 0x00000190473A7790>, ('Z:\\WIND\\GRAPES\\2024\\GRAPES_2024050100_240h_UV.nc',), 'r', (('mmap', None), ('version', 2)), '6432fc26-d2f4-4ff2-8c72-c83c40ef2b3a']
+            During handling of the above exception, another exception occurred:
+            ——————————————————————————
+            ValueError: When changing to a larger dtype, its size must be a divisor of the total size in bytes of the last axis of the array.
+            python-BaseException
+    """
+    # step1:批量读取nc文件并提取指定经纬度的72小时数据并拼接成 dataframe
+    # for index in range(11):
+    #     temp_month = index + 1
+    #     batch_readncfiles_byyears(read_path, lat, lng, 2024, temp_month)
+
+    # step2:将上面step1处理的按月保存的72小时浮标站位的时序数据合并为一整年的数据
+    merge_df = merge_dataframe(r'G:\05DATA\01TRAINING_DATA\WIND')
+    merge_df.to_csv(r'G:\05DATA\01TRAINING_DATA\WIND\merge.csv')
 
     read_file_full_path: str = r'G:\05DATA\01TRAINING_DATA\FUB\MF01001\2024_local.csv'
-    list_series = batch_get_realdata(read_file_full_path)
+    # step1: 生成一年的 365*2 =730 个 ws,ybg -> 只取 ws
+    df = batch_get_realdata(read_file_full_path)
+    # step2: 只提取 ws
+    df.to_csv(out_put_file_path)
+
     # 暂时注释掉批量生成file
     # for index in range(11):
     #     temp_month = index + 1
